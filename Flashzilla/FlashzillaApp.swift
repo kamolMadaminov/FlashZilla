22665b283e5d6540f482aa43c5ca225c94a69974
developerkamol <developerkamol@gmail.com>
2025-04-24 21:44:24 +0500

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Kamol Madaminov on 24/04/25.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
