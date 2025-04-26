a119319715525ad0848ce15b92eb69142a79eaa4
developerkamol <developerkamol@gmail.com>
2025-04-26 22:51:09 +0500

    Initial commit

---
+++ Flashzilla/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Kamol Madaminov on 24/04/25.
+//
+
+import Foundation
+
+struct Card{
+    var prompt: String
+    var answer: String
+    
+    static let example = Card(prompt: "Who played the 13th Doctor on Doctor Who?", answer: "Jodie Whittaker")
+}
